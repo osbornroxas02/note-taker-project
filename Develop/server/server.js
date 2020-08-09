daf7baa4974ecb9520016dd6b76eeb843c0e4504
osbornroxas <osbornroxas02@gmail.com>
2020-08-09 12:06:43 -0700

    final code

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const notes = require("../db/db.json");
-const port = 3000;
-var app = express();
-
+const path = require('path');
+const fs = require('fs');
 const bodyParser = require("body-parser");
-app.use(bodyParser.json()
-)
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const port = 3000;
+const app = express();
+
+app.use(bodyParser.json())
+app.use(express.static('../public'))
+
+// viewed at http://localhost:8080 use to connect frontend
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/../public/index.html'));
+});
+
+app.get('/notes', function (req, res) {
+    res.sendFile(path.join(__dirname + '/../public/notes.html'));
+});
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
@@ -17,10 +25,13 @@ app.get('/api/notes', (req, res) => {
 
 app.post('/api/notes', (req, res) => {
     const newNote = req.body
+    newNote.id = Date.now().toString();//unique id
     notes.push(newNote);
+    writeFile(JSON.stringify(notes));//update after +1
     res.json({
         success: true
     })
+    console.log(newNote);
 })
 
 app.delete('/api/notes/:id', (req, res) => {
@@ -28,7 +39,7 @@ app.delete('/api/notes/:id', (req, res) => {
     const deleteIndex = notes.findIndex(item => {
         return item.id === id
     });
-    if(deleteIndex === -1) {
+    if (deleteIndex === -1) {
         res.json({
             success: false,
             message: "ID doesn't exist"
@@ -36,13 +47,12 @@ app.delete('/api/notes/:id', (req, res) => {
     }
     else {
         notes.splice(deleteIndex, 1);
+        writeFile(JSON.stringify(notes));//update after delete
         res.json({
             success: true
         })
     }
-    res.send('delete notes' + id)
 })
-
 
 
 //starting server: last
@@ -51,5 +61,17 @@ app.listen(port, () => {
 })
 
 
-
-//saving notes
+//saving to file
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('../db/db.json',fileContent, err => {
+            if (err){
+                reject(err);
+            }
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    });
+};
